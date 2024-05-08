6adde6bbd66ac6dfeb2f69799a17cc4eff99d411
Ankita092005 <147826903+Ankita092005@users.noreply.github.com>
2024-05-08 16:14:22 +0000

    conversion of datatypes

---
+++ basics/03_conversion_operation.js
@@ -0,0 +1,26 @@
+// let score="33"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let abc=Number(score);
+// console.log(abc)
+
+let isLoggedIn = "Ankita"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ankita" => true
+
+
+let any_number=7
+
+let str_number=String(any_number)
+
+console.log(str_number)
+
+console.log(typeof(str_number))
+
